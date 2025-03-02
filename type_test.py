79fd5d5abd385209fe5044da1faacd3cf08d1973
Vince Swu <106900369+ecnivs@users.noreply.github.com>
2025-03-02 10:19:25 +0530

    Delete type_test.py

--- type_test.py
+++
@@ -1,8 +0,0 @@
-while True:
-    inn = input("-> ").lower()  # Convert input to lowercase for case insensitivity
-    if any(word in inn for word in ("start", "run", "simul")) and any(word in inn for word in ("attack", "test", "simul")):
-        print("YES")
-        continue
-    print("NO")
-
-
